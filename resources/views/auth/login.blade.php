523436be31084f53ad189e396546da49e0baff60
Rodrigo Affonso <raffonso1978@gmail.com>
2023-03-24 16:44:34 -0300

    Nova tela de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,35 +1,22 @@
 @extends('layouts.login')
 
 @section('content')
-  <div class="container mt-5">
-    <div class="row justify-content-center">
+<div class="container">
+    <div class="row justify-content-center mt-5">
         <div class="col-md-6">
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="text-center">Login</h3>
+            <form class="border p-4 rounded" method="post" action="{{ route('login.login') }}">
+                @csrf
+                <h3 class="text-center mb-4">Login</h3>
+                <div class="mb-3">
+                    <label for="email" class="form-label">E-mail</label>
+                    <input type="email" class="form-control" id="email" name="email">
                 </div>
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login.login') }}">
-                        @csrf
-                        <div class="form-group">
-                            <label for="email">E-mail</label>
-                            <input type="email" name="email" class="form-control" id="email">
-                        </div>
-                        <div class="form-group">
-                            <label for="password">Password</label>
-                            <input type="password" name="password" class="form-control" id="password">
-                        </div>
-                        {{-- <div class="form-check">
-                            <input type="checkbox" class="form-check-input" id="remember">
-                            <label class="form-check-label" for="remember">Remember me</label>
-                        </div> --}}
-                        <button type="submit" class="btn btn-primary d-block w-100 mt-3">Login</button>
-                    </form>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Senha</label>
+                    <input type="password" class="form-control" id="password" name="password">
                 </div>
-                {{--<div class="card-footer">
-                    <p class="text-center">Don't have an account? <a href="#">Register now</a></p>
-                </div> --}}
-            </div>
+                <button type="submit" class="btn btn-primary d-block w-100">Login</button>
+            </form>
         </div>
     </div>
 </div>
--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -6,13 +6,9 @@
     <link rel="stylesheet" href="/css/bootstrap.min.css">
 </head>
 <body>
-    @yield('content')
 
+@yield('content')
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script> -->
-<script src="/js/bootstrap.min.js"></script>
+<script src="/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
-
-
