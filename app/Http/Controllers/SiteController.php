818bf2ee1427b6d4c089c73c9bf4f6b1cfae1996
Rodrigo Affonso <raffonso1978@gmail.com>
2023-03-25 21:32:10 -0300

    Consulta cep

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Models\Tecnologia;
 use App\Models\SiteContato;
+use App\Models\Cep;
 
 class SiteController extends Controller
 {
@@ -22,5 +23,33 @@ class SiteController extends Controller
         SiteContato::create($dados);
 
         dd($dados);
+    }
+
+    public function cep($cep)
+    {
+
+        // $response = Cep::consultaCep($cep);
+
+        // dd($response);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'http://localhost:8000?cep=' . $cep,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = json_decode(curl_exec($curl));
+
+
+        curl_close($curl);
+        dd($response);
+
     }
 }
